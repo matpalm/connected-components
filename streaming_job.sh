3cff4524cddd22e38df402368b436d5d89396ef7
mat kelcey <matthew.kelcey@gmail.com>
2011-02-10 14:54:41 +1100

    iterative version of streaming job

--- streaming_job.sh
+++ streaming_job.sh
@@ -1,7 +1,7 @@
 set -x
 hadoop jar /usr/local/hadoop/contrib/streaming/hadoop-0.20.2-streaming.jar \
- -input input \
- -output output \
+ -input $1 \
+ -output $2 \
  -file emit_cc_to_neighbours.rb \
  -mapper emit_cc_to_neighbours.rb \
  -file min_cc_id_from_neighbours.rb \
